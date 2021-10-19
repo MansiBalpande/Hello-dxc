846135660391f3a11c68cd8bd4d461ad5d616b38
admin <admin@DESKTOP-K9JQ284>
2021-10-19 12:42:25 +0530

    all statements

---
+++ Hello dxc/src/controlstatements/IfElseExample.java
@@ -0,0 +1,16 @@
+package controlstatements;
+
+public class IfElseExample {
+	
+	public static void main(String[]) args) {
+		int number = 13;
+		
+		if(number %2 == 0) {
+			System.out.println("even no");
+		}
+		else {
+			System.out.println("odd no");
+		}
+	}
+
+}
