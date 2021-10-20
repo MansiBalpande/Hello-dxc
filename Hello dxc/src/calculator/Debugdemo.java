8b8605d897e913ecd7a2f14ad9cfb4dafd912f81
admin <admin@DESKTOP-K9JQ284>
2021-10-20 14:37:50 +0530

    all

--- Hello dxc/src/calculator/Debugdemo.java
+++ Hello dxc/src/calculator/Debugdemo.java
@@ -7,12 +7,16 @@ public class Debugdemo {
 		Calculator calc = new Calculator();
 		int sum = calc.add(10, 20);
 		System.out.println("the sum is--"+sum);
+		//Student abdul = new Student("ansari",123);
+		//Student myStudent = new Student();
+		//Student yourStudent = new Student ("mansi");
 		
 		for(int a=0; a<5; a++) {
+			int j = a*2;
 			int f = a+ 5 *3;
-			//System.out.println("the no is="+f);
+			System.out.println("the no is="+f);
 		}
 		
-		//throw new NullPointerException();
+		
 	}
 }
