d2490b90a51ce8d230f7bc434d01a9b0b076ef0b
admin <admin@DESKTOP-K9JQ284>
2021-10-20 11:19:58 +0530

    all statements

---
+++ Hello dxc/src/students/Constructor.java
@@ -0,0 +1,23 @@
+package students;
+
+public class Constructor {
+    String name;
+    int age;
+    
+    /** 
+     * default constructor -- Mansi
+     */
+    public student() {}
+
+	public Constructor(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+    /**
+    * shiny constructor-
+    * @param name
+    * @param age
+    */
+    
+}
