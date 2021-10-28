4d88dca961d432979557b737395b36f3db57d921
admin <admin@DESKTOP-K9JQ284>
2021-10-28 16:50:21 +0530

    streams

---
+++ Hello dxc/src/streams/BufferedInputStreamExample.java
@@ -0,0 +1,21 @@
+package streams;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+public class BufferedInputStreamExample {
+
+	public static void main(String args[]){    
+		  try{    
+		    FileInputStream fin=new FileInputStream("D:\\testout.txt");    
+		    BufferedInputStream bin=new BufferedInputStream(fin);    
+		    int i;    
+		    while((i=bin.read())!=-1){    
+		     System.out.print((char)i);    
+		    }    
+		    bin.close();    
+		    fin.close();    
+		  }catch(Exception e){System.out.println(e);}    
+		 }    
+		
+}
